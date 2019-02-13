75edaabc4d3a678e1eb540997b6973b1bee989a9
xiaopeng-pyhon <wuxiaoyanhenni@163.com>
2019-02-13 15:12:51 +0800

    201902131512

--- booktest/admin.py
+++ booktest/admin.py
@@ -5,8 +5,24 @@ from booktest.models import BookInfo,HeroInfo
 # Register your models here.
 #注册类模型
 
+class AreaStackedInline(admin.StackedInline):
+    model = AreaInfo
+    extra = 2
+
 class BookInfoAdmin(admin.ModelAdmin):
-    list_display = ['id', 'btile', 'bpub_data']
+    # 显示的属性字段
+    list_display = ['id', 'btile', 'bpub_data', 'title']
+    # 指定显示每页的条数
+    list_per_page = 10
+    actions_on_bottom = True
+    list_filter = ['atitle'] # 列表页右侧过滤栏
+    search_fields = ['atitle'] # 列表页上方的搜索框
+
+    fields = ['aParent', 'atitle']
+    fieldsets = (
+        ('基本', {'fields': ['atitle']}),
+        ('高级', {'fields': ['aParent']})
+    )
 
 class HeroInfoAdmin(admin.ModelAdmin):
     list_display = ['hname', 'hgender', 'hcommnet', 'hbook']
--- booktest/models.py
+++ booktest/models.py
@@ -4,3 +4,26 @@ from django.db import models
 
 # Create your models here.
 
+class AreaInfo(models.Model):
+    # 地区名称
+    atitle = models.CharField(verbose_name="标题", max_length=20)
+    # 自关联属性
+    aParent = models.ForeignKey('self', null=True, blank=True)
+
+    # 重写属性
+    def __str__(self):
+        return self.atitle
+
+
+    def title(self):
+        return self.atitle
+    # 指定可以排序的字段
+    title.admin_order_field = 'atitle'
+    # 指定中文名称
+    title.show_description = '地区名称'
+
+    def parent(self):
+        if self.aParent is None:
+            return ''
+        return self.aParent.atitle
+    parent.short_description = '父级地区名称'
